7e30518db9abf79621b90438cca33c8dc780d25e
stefan.karamanolev <stefan.karamanolev@kiteworks.com>
2026-01-23 12:01:06 +0200

    two strings challenge example solution

---
+++ src/main/java/com/kw/javachallenges/twostrings/stefan/TwoStringsSolution.java
@@ -0,0 +1,26 @@
+package com.kw.javachallenges.twostrings.stefan;
+
+import com.kw.javachallenges.twostrings.TwoStringsSolver;
+
+public class TwoStringsSolution implements TwoStringsSolver {
+
+    public static void main(String[] args) {
+        TwoStringsSolution solver = new TwoStringsSolution();
+
+        System.out.println("=============================================");
+        System.out.println("Testing Two Strings Implementation");
+        System.out.println("=============================================");
+
+        System.out.println("hasCommonSubstring(\"hello\", \"world\"): " + solver.hasCommonSubstring("hello", "world"));
+        System.out.println("hasCommonSubstring(\"hi\", \"world\"): " + solver.hasCommonSubstring("hi", "world"));
+        System.out.println("hasCommonSubstring(\"abc\", \"def\"): " + solver.hasCommonSubstring("abc", "def"));
+        System.out.println("hasCommonSubstring(\"\", \"hello\"): " + solver.hasCommonSubstring("", "hello"));
+
+        System.out.println("=============================================");
+    }
+
+    @Override
+    public boolean hasCommonSubstring(String s1, String s2) {
+        return false;
+    }
+}
