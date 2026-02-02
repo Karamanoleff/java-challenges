f277585d69d9ae997b9dcde8aaef67b12121d1ab
nevena.nesheva <neshevanevenka@gmail.com>
2026-02-02 07:36:45 +0200

    nnesheva twoStrings solution - corrected

--- src/main/java/com/kw/javachallenges/twostrings/nevenaNeshevaTest/TwoStringsSolution.java
+++ src/main/java/com/kw/javachallenges/twostrings/nevenaNeshevaTest/TwoStringsSolution.java
@@ -1,31 +1,17 @@
 package com.kw.javachallenges.twostrings.nevenaNeshevaTest;
 
+import com.kw.javachallenges.twostrings.TwoStringsSolver;
+
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
-public class TwoStringsSolution {
+public class TwoStringsSolution implements TwoStringsSolver {
     private static final String VALID_STRING_REGEX = "[a-z]+";
-
-    public static String twoStrings(String s1, String s2) {
-        Set<Character> characters = new HashSet<>();
-
-        for (int i = 0; i < s1.length(); i++) {
-            characters.add(s1.charAt(i));
-        }
-
-        for (int i = 0; i < s2.length(); i++) {
-            if (characters.contains(s2.charAt(i))) {
-                return "YES";
-            }
-        }
-
-        return "NO";
-    }
 
     private static int readValidTestCasesCount(Scanner scanner) {
         while (true) {
-            System.out.print("Enter number of test cases: ");
+            System.out.print("Enter number of pairs: ");
 
             if (scanner.hasNextInt()) {
                 int p = scanner.nextInt();
@@ -58,18 +44,40 @@ public class TwoStringsSolution {
         }
     }
 
+    @Override
+    public boolean hasCommonSubstring(String s1, String s2) {
+        Set<Character> characters = new HashSet<>();
+
+        for (int i = 0; i < s1.length(); i++) {
+            characters.add(s1.charAt(i));
+        }
+
+        for (int i = 0; i < s2.length(); i++) {
+            if (characters.contains(s2.charAt(i))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int p = readValidTestCasesCount(scanner);
+        int numberOfPairs = readValidTestCasesCount(scanner);
+        TwoStringsSolution solution = new TwoStringsSolution();
 
-        for (int i = 1; i <= p; i++) {
+        for (int i = 1; i <= numberOfPairs; i++) {
             System.out.println("\nTest case " + i + ":");
 
             String s1 = readValidString(scanner, "s1");
             String s2 = readValidString(scanner, "s2");
 
-            System.out.println("Result: " + twoStrings(s1, s2));
+            if (solution.hasCommonSubstring(s1, s2)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
         }
 
         scanner.close();
