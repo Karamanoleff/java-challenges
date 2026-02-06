4cd61b33ff83cc5817c61e63335c4269436f90c8
justin.daisenberger <j.daisenberger@dracoon.com>
2026-02-06 08:30:45 +0100

    implement AnagramPairsCounter

---
+++ src/main/java/com/kw/javachallenges/sherlockanagrams/justindaisenberger/JustinSherlockAnagrams.java
@@ -0,0 +1,57 @@
+package com.kw.javachallenges.sherlockanagrams.justindaisenberger;
+
+import com.kw.javachallenges.sherlockanagrams.AnagramPairsCounter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class JustinSherlockAnagrams implements AnagramPairsCounter {
+
+    @Override
+    public int countAnagrammaticPairs(String s) {
+
+        System.out.println("String: " + s);
+
+        if(s==null) throw new IllegalArgumentException();
+        if(s.length()<2) return 0;
+
+        int pairCount = 0;
+
+        List<String> allSubstrings = getSubstrings(s);
+        // System.out.println(allSubstrings);
+
+        for (int i = 0; i < allSubstrings.size(); i++) {
+            for (int j = i + 1; j < allSubstrings.size(); j++) {
+                if(isAnagramPair(allSubstrings.get(i), allSubstrings.get(j))) {
+                    System.out.println(allSubstrings.get(i) + " " + allSubstrings.get(j));
+                    pairCount++;
+                }
+            }
+        }
+
+        return pairCount;
+    }
+
+    private List<String> getSubstrings (String s) {
+
+        List <String> extractedSubstrings = new ArrayList<>();
+
+        for(int i = 0; i < s.length(); i++) {
+            for(int j = i + 1; j <= s.length(); j++) {
+                extractedSubstrings.add(s.substring(i, j));
+            }
+        }
+
+        return extractedSubstrings;
+    }
+
+    private boolean isAnagramPair(String s1, String s2) {
+        if(s1.length() != s2.length()) return false;
+        char[] arr1 = s1.toCharArray();
+        Arrays.sort(arr1);
+        char[] arr2 = s2.toCharArray();
+        Arrays.sort(arr2);
+        return Arrays.equals(arr1, arr2);
+    }
+}
