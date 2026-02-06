649d28327c78ed7e88c6bcf600a74de68ed05f09
stefan.karamanolev <stefan.karamanolev@kiteworks.com>
2026-02-06 15:32:08 +0200

    refactor anagrams tests

---
+++ src/main/java/com/kw/javachallenges/sherlockanagrams/stefan/SherlockAndAnagramsSolution.java
@@ -0,0 +1,15 @@
+package com.kw.javachallenges.sherlockanagrams.stefan;
+
+import com.kw.javachallenges.sherlockanagrams.AnagramPairsCounter;
+
+public class SherlockAndAnagramsSolution implements AnagramPairsCounter {
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public int countAnagrammaticPairs(String s) {
+        return 0;
+    }
+}
